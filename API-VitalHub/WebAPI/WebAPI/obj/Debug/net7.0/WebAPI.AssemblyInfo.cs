c188ff6b2e6154706ec5af25dff890014c5caeba
zAlves31 <alvesbautistajoaovictor@gmail.com>
2024-04-19 09:50:39 -0300

    Ocr feito

--- API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ef77ea6712df68298660aedfd91cd2b9e772bdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b31f04391902aa21bd73d30ea7ac0e134a5f88")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
