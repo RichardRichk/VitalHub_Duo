b34d49460594eca590c75e8c4655c21ebac09dc7
zAlves31 <alvesbautistajoaovictor@gmail.com>
2024-04-22 10:54:48 -0300

    telas de validacao de codigo do email

--- VitalHub/src/screens/ForgotPassword/ForgotPassword.js
+++ VitalHub/src/screens/ForgotPassword/ForgotPassword.js
@@ -5,10 +5,24 @@ import { Input } from "../../components/Input/Style"
 import { Logo, ReturnIcon } from "../../components/Logo/Style"
 import { SubTitle, Title } from "../../components/Title/Style"
 import LoadingButton from "../../utils/LoadingButton"
+import api from "../../Service/Service"
 
 export const ForgotPassword = ({ navigation }) => {
 
+    const [email, setEmail] = useState('alvesbautistajoaovictor@gmail.com')
+
+    async function EnviarEmail(){
+        await api.post(`/RecuperarSenha?email=${email}`)
+        .then(()=> {
+            navigation.replace("Email_Verify",{emailRecuperacao : email})
+        }).catch( error => {
+            console.log(error);
+        })
+
+    }
+
     const [loading, setLoading] = useState(false);
+
 
     // Função para cancelar a consulta
     const forgotPassword = async () => {
@@ -18,6 +32,8 @@ export const ForgotPassword = ({ navigation }) => {
             await new Promise(resolve => setTimeout(resolve, 800));
             navigation.replace("Email_Verify")
             setLoading(false);
+            EnviarEmail();
+
 
         } catch (error) {
             console.error("Erro ao cancelar consulta:", error);
@@ -50,6 +66,8 @@ export const ForgotPassword = ({ navigation }) => {
 
             <Input
                 placeholder="Usuário ou E-mail"
+                value={email}
+                onChangeText={(txt => setEmail(txt))}
             />
 
             <LoadingButton
